6751f0533f08f8af0bd55ba1a2e3e4e5dc8284ac
Md Tasrif Khan <tasrif.k.56@gmail.com>
2026-04-05 01:56:56 +0600

    navbar fixed

--- components/Home/Navbar.tsx
+++ components/Home/Navbar.tsx
@@ -8,32 +8,32 @@ import { ThemeToggle } from '@/components/theme-toggle';
 
 export default function Navbar() {
   return (
-    <div className="overflow-hidden fixed top-0 left-0 w-full h-[12vh] flex border items-center px-4 z-10000 transition-all duration-300 backdrop-blur-xl">
+    <div className="overflow-hidden fixed top-0 left-0 w-full h-[12vh] flex items-center px-4 z-10000 transition-all duration-300 backdrop-blur-xl">
       <Image src="/logo.jpg" alt="logo" height={40} width={40} />
 
       <div className="relative">
         <Link href="/">
-        <h6 className="ml-3 text-black dark:text-white text-md font-bold">
-        Dua <span className="text-emerald-600">&</span> Ruqyah
-        </h6>
-        <p className="text-gray-600 dark:text-gray-300 ml-3">Hisnul Muslim</p>
+          <h6 className="ml-3 text-black dark:text-white text-md font-bold">
+            Dua <span className="text-emerald-600">&</span> Ruqyah
+          </h6>
+          <p className="text-gray-600 dark:text-gray-300 ml-3">Hisnul Muslim</p>
         </Link>
       </div>
 
       {/* center: nav links */}
       <div className="flex-1 flex items-center justify-center space-x-6 font-bold text-gray-800 dark:text-gray-200">
         {NavLinks.map((link) => (
-         <Link key={link.id} href={link.url} className="relative overflow-hidden h-6 flex flex-col group cursor-pointer">
+          <Link key={link.id} href={link.url} className="relative overflow-hidden h-6 flex flex-col group cursor-pointer">
             <span className="transition-transform duration-300 group-hover:-translate-y-full">{link.name}</span>
             <span className="top-full transition-transform duration-300 group-hover:-translate-y-full hover:text-emerald-700">{link.name}</span>
-        </Link>
+          </Link>
         ))}
       </div>
 
       {/* right: future content placeholder */}
       <div className="flex items-center space-x-4">
         {/* put right-side items here */}
-        
+
         <Button className="px-6 py-2 text-sm md:text-md text-white font-bold bg-emerald-600 cursor-pointer hover:bg-emerald-600">
           <Link href="/login" className="relative overflow-hidden flex flex-col group h-6">
             <span className="transition-transform duration-300 group-hover:-translate-y-full">LogIn</span>
@@ -46,7 +46,7 @@ export default function Navbar() {
           placeholder="Search..."
           className="w-48 border-rounded border-emerald-900 bg-white dark:bg-black dark:text-white dark:border-emerald-600 dark:placeholder-gray-400"
         />
-        </div>
+      </div>
     </div>
   )
 }
